cde54c15a5820e174319e00d2d516e746bf0c4aa
Rahul Moje <rahulmoje@gmail.com>
2020-09-04 01:34:12 +0530

    Skeleton

---
+++ web/src/main/java/com/myorganisation/transfers/footballtransfers/FootballTransfersApplication.java
@@ -0,0 +1,13 @@
+package com.myorganisation.transfers.footballtransfers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballTransfersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballTransfersApplication.class, args);
+    }
+
+}
---
+++ web/src/test/java/com/myorganisation/transfers/footballtransfers/FootballTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myorganisation.transfers.footballtransfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballTransfersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
