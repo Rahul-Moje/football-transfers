4d5f17c30c8aebfde1fd5c001def39f3cd8753b7
Rahul Moje <rahulmoje@gmail.com>
2020-09-09 22:31:32 +0530

    Removing comment

--- web/src/main/java/com/myorganisation/transfers/footballtransfers/NewsExtractorScheduler.java
+++ web/src/main/java/com/myorganisation/transfers/footballtransfers/NewsExtractorScheduler.java
@@ -1,6 +1,6 @@
 package com.myorganisation.transfers.footballtransfers;
 
-import org.springframework.scheduling.annotation.EnableScheduling;
+
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
